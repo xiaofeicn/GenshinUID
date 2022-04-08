be07cc2e6a313263c1935267fc0a25894af8709a
xiaofeicn <271986756@qq.com>
2022-04-08 14:04:57 +0800

    去除命令 gs

--- genshin_uid/main.py
+++ genshin_uid/main.py
@@ -35,10 +35,10 @@ get_mys_info = on_startswith("mys", permission=GROUP, priority=priority)
 
 get_event = on_command('活动列表', priority=priority)
 get_lots = on_command('御神签', priority=priority)
-get_help = on_command('help',aliases={'gs帮助'}, priority=priority)
+get_help = on_command('help',aliases={'帮助'}, priority=priority)
 
-open_switch = on_startswith('gs开启', priority=priority)
-close_switch = on_startswith('gs关闭', priority=priority)
+open_switch = on_startswith('开启', priority=priority)
+close_switch = on_startswith('关闭', priority=priority)
 
 link_mys = on_startswith('绑定mys', priority=priority)
 link_uid = on_startswith('绑定uid', priority=priority)
@@ -503,7 +503,7 @@ async def add_cookie_func(bot: Bot, event: MessageEvent):
 async def open_switch_func(bot: Bot, event: MessageEvent):
     try:
         message = str(event.get_message()).strip().replace(
-            ' ', '').replace('gs开启', '')
+            ' ', '').replace('开启', '')
         m = ''.join(re.findall('[\u4e00-\u9fa5]', message))
 
         qid = int(event.sender.user_id)
@@ -583,7 +583,7 @@ async def open_switch_func(bot: Bot, event: MessageEvent):
 async def close_switch_func(bot: Bot, event: MessageEvent):
     try:
         message = str(event.get_message()).strip().replace(
-            ' ', '').replace('gs关闭', '')
+            ' ', '').replace('关闭', '')
         m = ''.join(re.findall('[\u4e00-\u9fa5]', message))
 
         qid = int(event.sender.user_id)
